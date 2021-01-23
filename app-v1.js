4128cf5e0a23765c7219bf72deba170a0d93aa42
Tommy Luu <tommy.1304@hotmail.com>
2021-01-23 21:46:59 +0100

    modified app-v1.js html

--- app-v1.js
+++ app-v1.js
@@ -4,12 +4,21 @@ const fs = require('fs');
 //vars
 const PORT = 8080;
 
+/**
+ * webserver wrapper
+ */
 const server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    sendFile('./home/home.html', res);
+    res.write('<h1>Welcome</h1>');
+    var headers = req.rawHeaders;
+    var headersHtml = htmlHeadersListAsString(headers)
+    res.write(headersHtml);
+    res.end();
 });
 
-
+/**
+ * enables server to listen to a port
+ */
 server.listen(PORT, function(error) {
     if(error) {
         console.log('Error occured', error);
@@ -18,6 +27,26 @@ server.listen(PORT, function(error) {
     }
 });
 
+/**
+ * input a raw array of strings (headers) and wrap with <ul><li> html
+ * @param {StringArray} headers 
+ */
+function htmlHeadersListAsString(headers) {
+    var buf = '<ul>';
+    var end = '</ul>';
+    for (i = 0; i<headers.length-1 ; i+=2) {
+        buf += '<li>';
+        buf += headers[i]+': '+headers[i+1];
+        buf += '</li>';
+    }
+    return buf+end;
+}
+
+/**
+ * read a file and write it to (response) object ex. index.html file
+ * @param {path to file} url 
+ * @param {response/request} object 
+ */
 function sendFile(url, object) {
     fs.readFile(url, function(error, data) {
         if(error) {
